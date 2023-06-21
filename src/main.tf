6362c0ca63c53315a2a26ea37676f2deae0d02e5
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-06-21 15:40:14 -0300

    First archive version

---
+++ src/main.tf
@@ -0,0 +1,106 @@
+# Configuração do Launch Configuration
+resource "aws_launch_template" "keycloak" {
+  name_prefix               = "keycloak"
+  image_id                  = var.ami_id
+  key_name                  = var.keyname
+  vpc_security_group_ids    = var.sg_keycloak
+  instance_type             = var.instance
+}
+
+# Configuração do Application Load Balancer
+resource "aws_lb" "alb_keycloak" {
+  name               = "ALB-Keycloaktf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.sg_alb_keycloak
+  subnets            = var.subnets
+
+  enable_deletion_protection = false
+}
+
+# Configuração do Target Group
+resource "aws_lb_target_group" "alb-keycloak" {
+  name        = "TG-Keycloatf"
+  port        = 8443
+  protocol    = "HTTPS"
+  vpc_id      = var.vpc_id
+     health_check {
+      healthy_threshold   = var.health_check["healthy_threshold"]
+      interval            = var.health_check["interval"]
+      unhealthy_threshold = var.health_check["unhealthy_threshold"]
+      timeout             = var.health_check["timeout"]
+      path                = var.health_check["path"]
+      port                = var.health_check["port"]
+  }
+}
+
+# Configuração dos Listeners
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb_keycloak.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "lb_listner_https" {
+  load_balancer_arn = aws_lb.alb_keycloak.id
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate
+
+  default_action {
+     type             = "forward"
+     target_group_arn = aws_lb_target_group.alb-keycloak.id
+  }
+}
+
+resource "aws_lb_listener_certificate" "cert_keycloak" {
+  listener_arn    = aws_lb_listener.lb_listner_https.arn
+  certificate_arn = var.certificate
+}
+
+# Configuração do Auto Scaling Group 
+resource "aws_autoscaling_group" "asg_keycloak" {
+  #availability_zones        = var.availability_zones
+  desired_capacity          = 1
+  max_size                  = 1
+  min_size                  = 1
+  health_check_grace_period = 300
+  target_group_arns         = ["${aws_lb_target_group.alb-keycloak.arn}"]
+  health_check_type         = "ELB"
+  vpc_zone_identifier       = var.subnets
+
+
+  launch_template {
+    id      = aws_launch_template.keycloak.id
+    version = "$Default"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.asg_keycloak.id
+  lb_target_group_arn    = aws_lb_target_group.alb-keycloak.arn
+}
+
+# Configuração da entrada A no Route 53
+resource "aws_route53_record" "www" {
+  zone_id = var.hostedzone
+  name    = "acessotf"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.alb_keycloak.dns_name
+    zone_id                = aws_lb.alb_keycloak.zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ src/variables.tf
@@ -0,0 +1,77 @@
+variable "region" {
+  default     = "us-east-2"
+  type        = string
+  description = "Region of the VPC"
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = "vpc-0b97505ab47807e80"
+  description = "vpc_keycloak"
+}
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-0378e324578ae75db"
+  description = "ami_id"
+}
+
+variable "sg_keycloak" {
+  type        = list
+  default     = ["sg-0f6a68253d49d98ae"]
+  description = "sg_keycloak"
+}
+
+variable "sg_alb_keycloak" {
+  type        = list
+  default     = ["sg-005fa7b45237fe42e"]
+  description = "sg_alb_keycloak"
+}
+
+variable "subnets" {
+  default     = ["subnet-0611591f91df44592", "subnet-009db7304cb9c1747", "subnet-00aed7fc8d3513a07"]
+  type        = list
+  description = "List of subnets"
+}
+
+variable "instance" {
+  type        = string
+  default     = "t3a.micro"
+  description = "instance_keycloak"
+}
+
+variable "keyname" {
+  type        = string
+  default     = "Key_Keycloak"
+  description = "instance_keycloak"
+}
+
+variable "availability_zones" {
+  default     = ["us-east-2a", "us-east-2b","us-east-2c"]
+  type        = list
+  description = "List of availability zones"
+}
+
+variable "certificate" {
+  type        = string
+  default     = "arn:aws:acm:us-east-2:906520347629:certificate/12094694-07df-47d7-bc2d-35c1ddec160c"
+  description = "certificate_keycloak"
+}
+
+variable "hostedzone" {
+  type        = string
+  default     = "Z3FOUYFF65CU70"
+  description = "hosted zone"
+}
+
+variable "health_check" {
+   type = map(string)
+   default = {
+      "timeout"  = "10"
+      "interval" = "20"
+      "path"     = "/"
+      "port"     = "80"
+      "unhealthy_threshold" = "2"
+      "healthy_threshold" = "3"
+    }
+}
